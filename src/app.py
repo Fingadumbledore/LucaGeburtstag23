94dcf39b4228ba659d4db455f0bceca050b4cbc2
hyperbel <hyperbel@users.noreply.github.com>
2023-02-25 23:39:18 +0100

    endpoints

--- src/app.py
+++ src/app.py
@@ -1,6 +1,36 @@
 """ Flask app for the application. """
 
 import json
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """ Render the index page. """
+    return render_template('index.html')
+
+@app.route('/caeser')
+def caeser():
+    """ Render the caeser page. """
+    return render_template('caeser.html')
+
+@app.route('/hilfe')
+def hilfe():
+    return render_template('hilfe.html')
+
+@app.route('/luftpumpe')
+def luftpumpe():
+    return render_template('luftpumpe.html')
+
+@app.route('/player')
+def player():
+    return render_template('player.html')
+
+@app.route('/videos')
+def videos():
+    return render_template('videos.html')
+
+@app.route('/werkzeugkoffer')
+def wekzeugkoffer():
+   return render_template('werkzeugkoffer.html')
